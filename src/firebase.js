854b45b372b4aecf65eec7f7e8629cb940e8b8ec
Nenosaj <113825727+Nenosaj@users.noreply.github.com>
2025-08-27 18:51:21 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,14 +8,14 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBHhYj4pQ6wWYbLFuSa80yJ5PBHGlgMJ-Q",
-  authDomain: "water-vendo-gcash.firebaseapp.com",
-  databaseURL: "https://water-vendo-gcash-default-rtdb.firebaseio.com/",
-  projectId: "water-vendo-gcash",
-  storageBucket: "water-vendo-gcash.firebasestorage.app",
-  messagingSenderId: "353067031897",
-  appId: "1:353067031897:web:ffccba6656b41057434048",
-  measurementId: "G-6F8HE4SG35"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
